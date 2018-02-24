9e40b86967a626f9fca618face98604b5d8c049b
Augustinas <augustinas.sueris@gmail.com>
2018-02-24 22:57:37 +0200

    Task2

--- src/lt/swedbank/carleasing/CarLeasing.java
+++ src/lt/swedbank/carleasing/CarLeasing.java
@@ -2,8 +2,24 @@ package lt.swedbank.carleasing;
 
 public class CarLeasing {
 
+    private static double AGREEMENT_FEE = 150;
+
     public static void main(String[] args) {
-        System.out.println("Car carPriceIncludingVat including VAT: " + Double.parseDouble(args[0]) + " EUR");
-        System.out.println("Down payment size : " + args[1] + "%");
+        double carPriceIncludingVat = Double.parseDouble(args[0]);
+        System.out.println("Car carPriceIncludingVat including VAT: " + carPriceIncludingVat + " EUR");
+
+        int downPaymentPercentage = Integer.parseInt(args[1]);
+        System.out.println("Down payment size : " + downPaymentPercentage + "%");
+        System.out.println();
+
+        double downPaymentAmount = carPriceIncludingVat * downPaymentPercentage / 100;
+        System.out.println("Down payment: " + downPaymentAmount + " EUR");
+        double loanSizeAmount = carPriceIncludingVat - downPaymentAmount;
+        System.out.println("Loan size: " + loanSizeAmount + " EUR");
+        System.out.println();
+
+        System.out.println("Agreement fee: " + AGREEMENT_FEE + " EUR");
+        double initialInstallmentAmount = downPaymentAmount + AGREEMENT_FEE;
+        System.out.println("Initial installment: " + initialInstallmentAmount + " EUR");
     }
 }
