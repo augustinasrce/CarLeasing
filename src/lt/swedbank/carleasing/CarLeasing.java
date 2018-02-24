bf218d45e2ed6ad0a302f25decd204741e78e258
Augustinas <augustinas.sueris@gmail.com>
2018-02-24 22:57:14 +0200

    Task1

--- src/lt/swedbank/carleasing/CarLeasing.java
+++ src/lt/swedbank/carleasing/CarLeasing.java
@@ -3,6 +3,7 @@ package lt.swedbank.carleasing;
 public class CarLeasing {
 
     public static void main(String[] args) {
-
+        System.out.println("Car carPriceIncludingVat including VAT: " + Double.parseDouble(args[0]) + " EUR");
+        System.out.println("Down payment size : " + args[1] + "%");
     }
 }
